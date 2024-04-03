8dc5ec3878107f4c3b1a2274b1774e9ace660751
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-04-04 00:24:13 +0700

    Like a post, list liked post

--- src/apis/postServices.js
+++ src/apis/postServices.js
@@ -1,5 +1,31 @@
 import axiosClient from './axiosClient'
 
-const postServices = {}
+const postServices = {
+  getAllPost(accessToken, page, size) {
+    const url = '/post/all?page=' + page + '&size=' + size
+    return axiosClient.get(url, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+  },
+
+  likePost(accessToken, postId) {
+    const url = '/post/' + postId + '/like'
+    return axiosClient.get(url, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+  },
+  getLikedPosts(accessToken, page, size) {
+    const url = '/post/liked?page=' + page + '&size=' + size
+    return axiosClient.get(url, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+  },
+}
 
 export default postServices
